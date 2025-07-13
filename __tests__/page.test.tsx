e08c39d8a141c6069a05c70d15c6cfbdd9b36f14
Subin <subinbs4a@gmail.com>
2025-07-13 22:25:44 +0300

    eb set up

--- __tests__/page.test.tsx
+++ __tests__/page.test.tsx
@@ -2,9 +2,40 @@ import { render, screen } from "@testing-library/react";
 import Home from "../src/app/page";
 import "@testing-library/jest-dom";
 
+// jest.mock("../src/utils/flagsmith",() => {
+//   return {
+//     getEnvironmentFlags: jest.fn(() => 
+//       Promise.resolve({
+//          isFeatureEnabled: () => {
+//           return true;
+//          }
+//       })),
+//   };
+// });
+
+// jest.mock("../src/utils/flagsmith.ts", () => ({
+//   getEnvironmentFlags: jest.fn(() => 
+//     Promise.resolve({
+//       isFeatureEnabled: jest.fn((feature) => {
+//         return feature === 'search';
+//       })
+//     })
+//   )
+// }));
+
+jest.mock('flagsmith-nodejs', () => ({
+  Flagsmith: jest.fn().mockImplementation(() => ({
+    getEnvironmentFlags: jest.fn(() => 
+      Promise.resolve({
+        isFeatureEnabled: jest.fn((feature) => feature === 'search')
+      })
+    )
+  }))
+}));
+
 describe("Home", () => {
-  it("renders a heading", () => {
-    render(<Home />);
+  it("renders a heading", async () => {
+    render(await Home());
 
     const docH = screen.getByRole("heading", {
       name: "Hello World",
