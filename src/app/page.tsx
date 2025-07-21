948e9ff87d9bed62c1fec59135c3e7dd385be49c
Subin <subinbs4a@gmail.com>
2025-07-21 22:52:28 +0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ export default async function Home() {
   return (
     <main className="main">
       <div>
-        <h2>Hello World5</h2>
+        <h2>Hello World6</h2>
       </div>
       <Link href="/about">About</Link>
       <br />
